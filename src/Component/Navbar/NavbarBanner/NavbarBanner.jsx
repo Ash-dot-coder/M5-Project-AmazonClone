969a22847e9753809c9997ba60511d762fe9b3ea
Ayush Kohre <kohreaayush@gmail.com>
2025-05-09 05:01:57 +0530

    AmazonCone[NavbarBanner] - 2

--- src/Component/Navbar/NavbarBanner/NavbarBanner.jsx
+++ src/Component/Navbar/NavbarBanner/NavbarBanner.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
 import './NavbarBanner.css';
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 
 const NavbarBanner = () => {
-  return (
-    <div>NavbarBanner</div>
-  )
+    const options = [
+        { "name": "Fresh" },
+        { "name": "Amazon miniT" },
+        { "name": "Sell" },
+        { "name": "Best Sellers" },
+        { "name": "Today's Deals" },
+        { "name": "Mobiles" },
+        { "name": "Electronics" },
+        { "name": "Prime" },
+        { "name": "Customer Service" },
+        { "name": "Fashion" },
+        { "name": "Home & Kitchen" }
+    ]
+
+    return (
+        <div className='navbarBanner'>
+            <div className="navbarBannerOptionsLeft">
+                <div className="optionNavbarBanner">
+                    <MenuOutlinedIcon sx={{ fontSize: '24px' }} />
+                    <div className="allOptionsNavbarBanner">All</div>
+                </div>
+
+                {
+                    options.map((item, ind) => {
+                        return (
+                            <div className="optionNavbarBanner" key={ind}>
+                                <div className="allOptionsNavbarBanner">{item.name}</div>
+                            </div>
+                        )
+                    })
+                }
+
+
+            </div>
+            <div className="navbarBannerRightSide">
+                <img src="https://f.media-amazon.com/images/G/31/img19/SiddMiniTV/26MayHero/SWM_400x391._CB557661637_.jpg" alt="AmazonPrimeLogo" />
+            </div>
+        </div>
+    )
 }
 
 export default NavbarBanner
