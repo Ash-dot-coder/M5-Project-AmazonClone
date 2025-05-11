64741e427e3950f8209b6d511113775d5e0731d8
Ayush Kohre <kohreaayush@gmail.com>
2025-05-11 14:09:32 +0530

    AmazonClone[Home - DetailSection] - 4

--- src/Screen/Home/HomeDetails/HomeDetails.jsx
+++ src/Screen/Home/HomeDetails/HomeDetails.jsx
@@ -11,238 +11,238 @@ const HomeDetails = () => {
 
           <div className="scrollDiv">
             <div className="homeDetialLongCardItem">
-  <img className='homeDetailLongCardItemImg' src='https://m.media-amazon.com/images/I/31ppZt0uIML._SR480,440_.jpg'/>
-  <div className='homeDetailLongCardItemImgDetail'>
+              <img className='homeDetailLongCardItemImg' src='https://m.media-amazon.com/images/I/31ppZt0uIML._SR480,440_.jpg' />
+              <div className='homeDetailLongCardItemImgDetail'>
 
-  <div className='homeDetailLongCardItemImgTopDetail'>
-<div className='homeDetailPercentageOff'>
-up to 17% off
-  </div>
-<div className='limitedTimeDealHomeDetail'>Limited Time Deal</div>
-</div>
+                <div className='homeDetailLongCardItemImgTopDetail'>
+                  <div className='homeDetailPercentageOff'>
+                    up to 17% off
+                  </div>
+                  <div className='limitedTimeDealHomeDetail'>Limited Time Deal</div>
+                </div>
 
-<div className="bottomHomeDetail">Nothing Phone (3A)(5G) | Starting @23768 Includ...</div>
-</div>
+                <div className="bottomHomeDetail">Nothing Phone (3A)(5G) | Starting @23768 Includ...</div>
+              </div>
             </div>
             <div className="homeDetialLongCardItem">
-  <img className='homeDetailLongCardItemImg' src='https://m.media-amazon.com/images/I/31ppZt0uIML._SR480,440_.jpg'/>
-  <div className='homeDetailLongCardItemImgDetail'>
+              <img className='homeDetailLongCardItemImg' src='https://m.media-amazon.com/images/I/31ppZt0uIML._SR480,440_.jpg' />
+              <div className='homeDetailLongCardItemImgDetail'>
 
-  <div className='homeDetailLongCardItemImgTopDetail'>
-<div className='homeDetailPercentageOff'>
-up to 17% off
-  </div>
-<div className='limitedTimeDealHomeDetail'>Limited Time Deal</div>
-</div>
+                <div className='homeDetailLongCardItemImgTopDetail'>
+                  <div className='homeDetailPercentageOff'>
+                    up to 17% off
+                  </div>
+                  <div className='limitedTimeDealHomeDetail'>Limited Time Deal</div>
+                </div>
 
-<div className="bottomHomeDetail">Nothing Phone (3A)(5G) | Starting @23768 Includ...</div>
-</div>
+                <div className="bottomHomeDetail">Nothing Phone (3A)(5G) | Starting @23768 Includ...</div>
+              </div>
             </div>
             <div className="homeDetialLongCardItem">
-  <img className='homeDetailLongCardItemImg' src='https://m.media-amazon.com/images/I/31ppZt0uIML._SR480,440_.jpg'/>
-  <div className='homeDetailLongCardItemImgDetail'>
+              <img className='homeDetailLongCardItemImg' src='https://m.media-amazon.com/images/I/31ppZt0uIML._SR480,440_.jpg' />
+              <div className='homeDetailLongCardItemImgDetail'>
 
-  <div className='homeDetailLongCardItemImgTopDetail'>
-<div className='homeDetailPercentageOff'>
-up to 17% off
-  </div>
-<div className='limitedTimeDealHomeDetail'>Limited Time Deal</div>
-</div>
+                <div className='homeDetailLongCardItemImgTopDetail'>
+                  <div className='homeDetailPercentageOff'>
+                    up to 17% off
+                  </div>
+                  <div className='limitedTimeDealHomeDetail'>Limited Time Deal</div>
+                </div>
 
-<div className="bottomHomeDetail">Nothing Phone (3A)(5G) | Starting @23768 Includ...</div>
-</div>
+                <div className="bottomHomeDetail">Nothing Phone (3A)(5G) | Starting @23768 Includ...</div>
+              </div>
             </div>
             <div className="homeDetialLongCardItem">
-  <img className='homeDetailLongCardItemImg' src='https://m.media-amazon.com/images/I/31ppZt0uIML._SR480,440_.jpg'/>
-  <div className='homeDetailLongCardItemImgDetail'>
+              <img className='homeDetailLongCardItemImg' src='https://m.media-amazon.com/images/I/31ppZt0uIML._SR480,440_.jpg' />
+              <div className='homeDetailLongCardItemImgDetail'>
 
-  <div className='homeDetailLongCardItemImgTopDetail'>
-<div className='homeDetailPercentageOff'>
-up to 17% off
-  </div>
-<div className='limitedTimeDealHomeDetail'>Limited Time Deal</div>
-</div>
+                <div className='homeDetailLongCardItemImgTopDetail'>
+                  <div className='homeDetailPercentageOff'>
+                    up to 17% off
+                  </div>
+                  <div className='limitedTimeDealHomeDetail'>Limited Time Deal</div>
+                </div>
 
-<div className="bottomHomeDetail">Nothing Phone (3A)(5G) | Starting @23768 Includ...</div>
-</div>
+                <div className="bottomHomeDetail">Nothing Phone (3A)(5G) | Starting @23768 Includ...</div>
+              </div>
             </div>
             <div className="homeDetialLongCardItem">
-  <img className='homeDetailLongCardItemImg' src='https://m.media-amazon.com/images/I/31ppZt0uIML._SR480,440_.jpg'/>
-  <div className='homeDetailLongCardItemImgDetail'>
+              <img className='homeDetailLongCardItemImg' src='https://m.media-amazon.com/images/I/31ppZt0uIML._SR480,440_.jpg' />
+              <div className='homeDetailLongCardItemImgDetail'>
 
-  <div className='homeDetailLongCardItemImgTopDetail'>
-<div className='homeDetailPercentageOff'>
-up to 17% off
-  </div>
-<div className='limitedTimeDealHomeDetail'>Limited Time Deal</div>
-</div>
+                <div className='homeDetailLongCardItemImgTopDetail'>
+                  <div className='homeDetailPercentageOff'>
+                    up to 17% off
+                  </div>
+                  <div className='limitedTimeDealHomeDetail'>Limited Time Deal</div>
+                </div>
 
-<div className="bottomHomeDetail">Nothing Phone (3A)(5G) | Starting @23768 Includ...</div>
-</div>
+                <div className="bottomHomeDetail">Nothing Phone (3A)(5G) | Starting @23768 Includ...</div>
+              </div>
             </div>
             <div className="homeDetialLongCardItem">
-  <img className='homeDetailLongCardItemImg' src='https://m.media-amazon.com/images/I/31ppZt0uIML._SR480,440_.jpg'/>
-  <div className='homeDetailLongCardItemImgDetail'>
+              <img className='homeDetailLongCardItemImg' src='https://m.media-amazon.com/images/I/31ppZt0uIML._SR480,440_.jpg' />
+              <div className='homeDetailLongCardItemImgDetail'>
 
-  <div className='homeDetailLongCardItemImgTopDetail'>
-<div className='homeDetailPercentageOff'>
-up to 17% off
-  </div>
-<div className='limitedTimeDealHomeDetail'>Limited Time Deal</div>
-</div>
+                <div className='homeDetailLongCardItemImgTopDetail'>
+                  <div className='homeDetailPercentageOff'>
+                    up to 17% off
+                  </div>
+                  <div className='limitedTimeDealHomeDetail'>Limited Time Deal</div>
+                </div>
 
-<div className="bottomHomeDetail">Nothing Phone (3A)(5G) | Starting @23768 Includ...</div>
-</div>
+                <div className="bottomHomeDetail">Nothing Phone (3A)(5G) | Starting @23768 Includ...</div>
+              </div>
             </div>
             <div className="homeDetialLongCardItem">
-  <img className='homeDetailLongCardItemImg' src='https://m.media-amazon.com/images/I/31ppZt0uIML._SR480,440_.jpg'/>
-  <div className='homeDetailLongCardItemImgDetail'>
+              <img className='homeDetailLongCardItemImg' src='https://m.media-amazon.com/images/I/31ppZt0uIML._SR480,440_.jpg' />
+              <div className='homeDetailLongCardItemImgDetail'>
 
-  <div className='homeDetailLongCardItemImgTopDetail'>
-<div className='homeDetailPercentageOff'>
-up to 17% off
-  </div>
-<div className='limitedTimeDealHomeDetail'>Limited Time Deal</div>
-</div>
+                <div className='homeDetailLongCardItemImgTopDetail'>
+                  <div className='homeDetailPercentageOff'>
+                    up to 17% off
+                  </div>
+                  <div className='limitedTimeDealHomeDetail'>Limited Time Deal</div>
+                </div>
 
-<div className="bottomHomeDetail">Nothing Phone (3A)(5G) | Starting @23768 Includ...</div>
-</div>
+                <div className="bottomHomeDetail">Nothing Phone (3A)(5G) | Starting @23768 Includ...</div>
+              </div>
             </div>
             <div className="homeDetialLongCardItem">
-  <img className='homeDetailLongCardItemImg' src='https://m.media-amazon.com/images/I/31ppZt0uIML._SR480,440_.jpg'/>
-  <div className='homeDetailLongCardItemImgDetail'>
+              <img className='homeDetailLongCardItemImg' src='https://m.media-amazon.com/images/I/31ppZt0uIML._SR480,440_.jpg' />
+              <div className='homeDetailLongCardItemImgDetail'>
 
-  <div className='homeDetailLongCardItemImgTopDetail'>
-<div className='homeDetailPercentageOff'>
-up to 17% off
-  </div>
-<div className='limitedTimeDealHomeDetail'>Limited Time Deal</div>
-</div>
+                <div className='homeDetailLongCardItemImgTopDetail'>
+                  <div className='homeDetailPercentageOff'>
+                    up to 17% off
+                  </div>
+                  <div className='limitedTimeDealHomeDetail'>Limited Time Deal</div>
+                </div>
 
-<div className="bottomHomeDetail">Nothing Phone (3A)(5G) | Starting @23768 Includ...</div>
-</div>
+                <div className="bottomHomeDetail">Nothing Phone (3A)(5G) | Starting @23768 Includ...</div>
+              </div>
             </div>
           </div>
 
         </div>
       </div>
-      
+
       <div className="homeDetailLongCard">
         <div className="homeDetailLongCardTitle">Today's Offer</div>
         <div className="homeDetailLongCardItems">
 
           <div className="scrollDiv">
             <div className="homeDetialLongCardItem">
-  <img className='homeDetailLongCardItemImg' src='https://m.media-amazon.com/images/I/31ppZt0uIML._SR480,440_.jpg'/>
-  <div className='homeDetailLongCardItemImgDetail'>
+              <img className='homeDetailLongCardItemImg' src='https://m.media-amazon.com/images/I/31ppZt0uIML._SR480,440_.jpg' />
+              <div className='homeDetailLongCardItemImgDetail'>
 
-  <div className='homeDetailLongCardItemImgTopDetail'>
-<div className='homeDetailPercentageOff'>
-up to 17% off
-  </div>
-<div className='limitedTimeDealHomeDetail'>Limited Time Deal</div>
-</div>
+                <div className='homeDetailLongCardItemImgTopDetail'>
+                  <div className='homeDetailPercentageOff'>
+                    up to 17% off
+                  </div>
+                  <div className='limitedTimeDealHomeDetail'>Limited Time Deal</div>
+                </div>
 
-<div className="bottomHomeDetail">Nothing Phone (3A)(5G) | Starting @23768 Includ...</div>
-</div>
+                <div className="bottomHomeDetail">Nothing Phone (3A)(5G) | Starting @23768 Includ...</div>
+              </div>
             </div>
             <div className="homeDetialLongCardItem">
-  <img className='homeDetailLongCardItemImg' src='https://m.media-amazon.com/images/I/31ppZt0uIML._SR480,440_.jpg'/>
-  <div className='homeDetailLongCardItemImgDetail'>
+              <img className='homeDetailLongCardItemImg' src='https://m.media-amazon.com/images/I/31ppZt0uIML._SR480,440_.jpg' />
+              <div className='homeDetailLongCardItemImgDetail'>
 
-  <div className='homeDetailLongCardItemImgTopDetail'>
-<div className='homeDetailPercentageOff'>
-up to 17% off
-  </div>
-<div className='limitedTimeDealHomeDetail'>Limited Time Deal</div>
-</div>
+                <div className='homeDetailLongCardItemImgTopDetail'>
+                  <div className='homeDetailPercentageOff'>
+                    up to 17% off
+                  </div>
+                  <div className='limitedTimeDealHomeDetail'>Limited Time Deal</div>
+                </div>
 
-<div className="bottomHomeDetail">Nothing Phone (3A)(5G) | Starting @23768 Includ...</div>
-</div>
+                <div className="bottomHomeDetail">Nothing Phone (3A)(5G) | Starting @23768 Includ...</div>
+              </div>
             </div>
             <div className="homeDetialLongCardItem">
-  <img className='homeDetailLongCardItemImg' src='https://m.media-amazon.com/images/I/31ppZt0uIML._SR480,440_.jpg'/>
-  <div className='homeDetailLongCardItemImgDetail'>
+              <img className='homeDetailLongCardItemImg' src='https://m.media-amazon.com/images/I/31ppZt0uIML._SR480,440_.jpg' />
+              <div className='homeDetailLongCardItemImgDetail'>
 
-  <div className='homeDetailLongCardItemImgTopDetail'>
-<div className='homeDetailPercentageOff'>
-up to 17% off
-  </div>
-<div className='limitedTimeDealHomeDetail'>Limited Time Deal</div>
-</div>
+                <div className='homeDetailLongCardItemImgTopDetail'>
+                  <div className='homeDetailPercentageOff'>
+                    up to 17% off
+                  </div>
+                  <div className='limitedTimeDealHomeDetail'>Limited Time Deal</div>
+                </div>
 
-<div className="bottomHomeDetail">Nothing Phone (3A)(5G) | Starting @23768 Includ...</div>
-</div>
+                <div className="bottomHomeDetail">Nothing Phone (3A)(5G) | Starting @23768 Includ...</div>
+              </div>
             </div>
             <div className="homeDetialLongCardItem">
-  <img className='homeDetailLongCardItemImg' src='https://m.media-amazon.com/images/I/31ppZt0uIML._SR480,440_.jpg'/>
-  <div className='homeDetailLongCardItemImgDetail'>
+              <img className='homeDetailLongCardItemImg' src='https://m.media-amazon.com/images/I/31ppZt0uIML._SR480,440_.jpg' />
+              <div className='homeDetailLongCardItemImgDetail'>
 
-  <div className='homeDetailLongCardItemImgTopDetail'>
-<div className='homeDetailPercentageOff'>
-up to 17% off
-  </div>
-<div className='limitedTimeDealHomeDetail'>Limited Time Deal</div>
-</div>
+                <div className='homeDetailLongCardItemImgTopDetail'>
+                  <div className='homeDetailPercentageOff'>
+                    up to 17% off
+                  </div>
+                  <div className='limitedTimeDealHomeDetail'>Limited Time Deal</div>
+                </div>
 
-<div className="bottomHomeDetail">Nothing Phone (3A)(5G) | Starting @23768 Includ...</div>
-</div>
+                <div className="bottomHomeDetail">Nothing Phone (3A)(5G) | Starting @23768 Includ...</div>
+              </div>
             </div>
             <div className="homeDetialLongCardItem">
-  <img className='homeDetailLongCardItemImg' src='https://m.media-amazon.com/images/I/31ppZt0uIML._SR480,440_.jpg'/>
-  <div className='homeDetailLongCardItemImgDetail'>
+              <img className='homeDetailLongCardItemImg' src='https://m.media-amazon.com/images/I/31ppZt0uIML._SR480,440_.jpg' />
+              <div className='homeDetailLongCardItemImgDetail'>
 
-  <div className='homeDetailLongCardItemImgTopDetail'>
-<div className='homeDetailPercentageOff'>
-up to 17% off
-  </div>
-<div className='limitedTimeDealHomeDetail'>Limited Time Deal</div>
-</div>
+                <div className='homeDetailLongCardItemImgTopDetail'>
+                  <div className='homeDetailPercentageOff'>
+                    up to 17% off
+                  </div>
+                  <div className='limitedTimeDealHomeDetail'>Limited Time Deal</div>
+                </div>
 
-<div className="bottomHomeDetail">Nothing Phone (3A)(5G) | Starting @23768 Includ...</div>
-</div>
+                <div className="bottomHomeDetail">Nothing Phone (3A)(5G) | Starting @23768 Includ...</div>
+              </div>
             </div>
             <div className="homeDetialLongCardItem">
-  <img className='homeDetailLongCardItemImg' src='https://m.media-amazon.com/images/I/31ppZt0uIML._SR480,440_.jpg'/>
-  <div className='homeDetailLongCardItemImgDetail'>
+              <img className='homeDetailLongCardItemImg' src='https://m.media-amazon.com/images/I/31ppZt0uIML._SR480,440_.jpg' />
+              <div className='homeDetailLongCardItemImgDetail'>
 
-  <div className='homeDetailLongCardItemImgTopDetail'>
-<div className='homeDetailPercentageOff'>
-up to 17% off
-  </div>
-<div className='limitedTimeDealHomeDetail'>Limited Time Deal</div>
-</div>
+                <div className='homeDetailLongCardItemImgTopDetail'>
+                  <div className='homeDetailPercentageOff'>
+                    up to 17% off
+                  </div>
+                  <div className='limitedTimeDealHomeDetail'>Limited Time Deal</div>
+                </div>
 
-<div className="bottomHomeDetail">Nothing Phone (3A)(5G) | Starting @23768 Includ...</div>
-</div>
+                <div className="bottomHomeDetail">Nothing Phone (3A)(5G) | Starting @23768 Includ...</div>
+              </div>
             </div>
             <div className="homeDetialLongCardItem">
-  <img className='homeDetailLongCardItemImg' src='https://m.media-amazon.com/images/I/31ppZt0uIML._SR480,440_.jpg'/>
-  <div className='homeDetailLongCardItemImgDetail'>
+              <img className='homeDetailLongCardItemImg' src='https://m.media-amazon.com/images/I/31ppZt0uIML._SR480,440_.jpg' />
+              <div className='homeDetailLongCardItemImgDetail'>
 
-  <div className='homeDetailLongCardItemImgTopDetail'>
-<div className='homeDetailPercentageOff'>
-up to 17% off
-  </div>
-<div className='limitedTimeDealHomeDetail'>Limited Time Deal</div>
-</div>
+                <div className='homeDetailLongCardItemImgTopDetail'>
+                  <div className='homeDetailPercentageOff'>
+                    up to 17% off
+                  </div>
+                  <div className='limitedTimeDealHomeDetail'>Limited Time Deal</div>
+                </div>
 
-<div className="bottomHomeDetail">Nothing Phone (3A)(5G) | Starting @23768 Includ...</div>
-</div>
+                <div className="bottomHomeDetail">Nothing Phone (3A)(5G) | Starting @23768 Includ...</div>
+              </div>
             </div>
             <div className="homeDetialLongCardItem">
-  <img className='homeDetailLongCardItemImg' src='https://m.media-amazon.com/images/I/31ppZt0uIML._SR480,440_.jpg'/>
-  <div className='homeDetailLongCardItemImgDetail'>
+              <img className='homeDetailLongCardItemImg' src='https://m.media-amazon.com/images/I/31ppZt0uIML._SR480,440_.jpg' />
+              <div className='homeDetailLongCardItemImgDetail'>
 
-  <div className='homeDetailLongCardItemImgTopDetail'>
-<div className='homeDetailPercentageOff'>
-up to 17% off
-  </div>
-<div className='limitedTimeDealHomeDetail'>Limited Time Deal</div>
-</div>
+                <div className='homeDetailLongCardItemImgTopDetail'>
+                  <div className='homeDetailPercentageOff'>
+                    up to 17% off
+                  </div>
+                  <div className='limitedTimeDealHomeDetail'>Limited Time Deal</div>
+                </div>
 
-<div className="bottomHomeDetail">Nothing Phone (3A)(5G) | Starting @23768 Includ...</div>
-</div>
+                <div className="bottomHomeDetail">Nothing Phone (3A)(5G) | Starting @23768 Includ...</div>
+              </div>
             </div>
           </div>
 
