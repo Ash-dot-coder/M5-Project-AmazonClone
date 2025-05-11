5093507029bac5724136e1eba02d76cb0ddffc39
Ayush Kohre <kohreaayush@gmail.com>
2025-05-11 14:08:02 +0530

    AmazonClone[Footer] - 5

--- src/Screen/Home/HomeScreen.jsx
+++ src/Screen/Home/HomeScreen.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './HomeScreen.css'
 import HomeBanner from './HomeBanner/HomeBanner'
 import HomeDetails from './HomeDetails/HomeDetails'
+import Footer from '../../Component/Footer/Footer'
 
 const HomeScreen = () => {
   return (
     <div className='homeScreen'>
       <HomeBanner />
       <HomeDetails />
+      <Footer />
     </div>
   )
 }
